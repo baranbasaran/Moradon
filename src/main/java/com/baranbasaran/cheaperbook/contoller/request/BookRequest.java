3977b6e8077b077a98b6bc686648da25aba8994d
Ertuğrul Berat ALLAHVERDİ <berat.allahverdi@hotmail.com>
2024-05-20 19:51:38 +0300

    request dto added

---
+++ src/main/java/com/baranbasaran/cheaperbook/contoller/request/BookRequest.java
@@ -0,0 +1,29 @@
+package com.baranbasaran.cheaperbook.contoller.request;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BookRequest {
+
+    private Long id;
+
+    @NonNull
+    private String title;
+
+    @NonNull
+    private String author;
+
+    @NonNull
+    private String genre;
+
+    @NonNull
+    private String description;
+
+    @NonNull
+    private String owner;
+
+    @NonNull
+    private Integer price;
+}
---
+++ src/main/java/com/baranbasaran/cheaperbook/contoller/request/UpdateBookRequest.java
@@ -0,0 +1,10 @@
+package com.baranbasaran.cheaperbook.contoller.request;
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class UpdateBookRequest extends BookRequest {
+}
